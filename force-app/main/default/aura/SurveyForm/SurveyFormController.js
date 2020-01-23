35ebedf1f75093f49426014e5cd226f7c75d6b39
pavlishinnikita <pavlishin.nikita@gmail.com>
2020-01-23 12:37:45 +0200

    Create survey page, component and  controller

--- force-app/main/default/aura/SurveyForm/SurveyFormController.js
+++ force-app/main/default/aura/SurveyForm/SurveyFormController.js
@@ -1,13 +1,15 @@
 ({
     doInit : function(component, event, helper) {
         let action = component.get("c.getInit");
+        action.setParams({
+            'trainingId': component.get('v.trainingId'),
+            'participantId': component.get('v.participantId')
+        });
         action.setCallback(this, (res) => {
             if(res.getState() == 'SUCCESS') {
                 console.log(res.getReturnValue());
-                component.set('v.trainingId', res.getReturnValue());
             } else {
-                // handle error
-                console.log('ERROR');
+                console.log(res.getError()[0].message);
             }
         });
         $A.enqueueAction(action);
